2a6051a15ce9927b1826b7e956c6b008a91b68a6
Prajwal _Ghadole <prajwalghadole@gmail.com>
2023-12-14 20:33:44 +0530

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -122,14 +122,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#STATICFILES_DIRS = [
-#    BASE_DIR / 'static'
-#]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
